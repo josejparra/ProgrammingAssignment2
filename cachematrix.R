add92e4c416e67fb63108a6086461782d79acdba
José Julián Parra <julianparra2103@gmail.com>
2019-02-09 13:50:22 -0500

    Edited as required

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+#This two functions take advantage of R's lexical scoping
+#to reuse an existing calculation, in this instance,
+#of the inverse of a matrix, instead of calculating
+#it from scratch
 
-## Write a short comment describing this function
+#This first function creates a list using a matrix object
+#defined as an input. This list contain four functions
+#to be used by the second function that actually calculates
+#the inverse or retrieves it if calculated previously.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse= getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#As specified before, this function takes as an argument
+#an objet of the type makeCacheMatrix, which is a list that
+#contains four different functions. Then it makes use
+#of those functions and calculates the inverse of a matrix
+#previously specified as an input in makeCacheMatrix, or
+#retrieves it if calculated previously.
+cachesolve <- function(x, ...) {
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
